4c013adf453415ff6cf50c54740954fee6e18fcd
Juan Diaz <juanchodis@hotmail.com>
2023-12-17 09:46:06 -0500

    [update] GET /random >>>>  To >>> GET /quotes/random based on API documentation: https://github.com/lukePeavey/quotable?tab=readme-ov-file#get-random-quotes

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,7 +9,8 @@ const Home = () => {
 
   // UseEffect is a hook to fetch the data from the API
   useEffect(() => {
-    fetch("https://api.quotable.io/random")
+    // fetch("https://api.quotable.io/random")
+    fetch("https://api.quotable.io/quotes/random")
       .then((response) => response.json())
       .then((json) => {
         console.log("Data from API: ", json); // Log the data
@@ -22,10 +23,11 @@ const Home = () => {
       <div className="flex flex-col justify-center items-center gap-4 w-screen h-screen">
         <h1 className="text-2xl font-bold">Quote Generator</h1>
         <div className="flex flex-col justify-normal items-start gap-3 shadow-md shadow-black rounded-lg p-6 m-6 bg-slate-300/20 w-[700px] h-48">
-          <p>{endpoints.content}</p>
-          <p>{endpoints.author}</p>
-          <p>{endpoints.length}</p>
-          <p>{endpoints.tags}</p>
+          {console.log("id: ", endpoints[0]?._id)}
+          <p>{endpoints[0]?.content}</p>
+          <p>{endpoints[0]?.lenght}</p>
+          <p>{endpoints[0]?.author}</p>
+          <p>{endpoints[0]?.tags}</p>
         </div>
       </div>
     </Layout>

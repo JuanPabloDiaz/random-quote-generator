d49c45f047d48361f38f7f5d1d09456a7db1f7f9
Juan Diaz <juanchodis@hotmail.com>
2023-12-17 11:20:01 -0500

    footer author

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -51,7 +51,7 @@ const Home = () => {
         </button>
         <div className="flex justify-end fixed bottom-10 w-5/6 gap-2">
           <p className="text-sm sm:text-md md:text-lg font-normal text-[#5f5f5f]">
-            Develop by:
+            Developed by:
           </p>
           <span className="text-sm sm:text-md md:text-lg font-normal text-[#999999]">
             Juan Diaz

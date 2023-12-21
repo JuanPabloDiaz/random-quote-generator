484b21ad3038f531be76ebf338a3cb0d2eefb20d
Juan Diaz <juanchodis@hotmail.com>
2023-12-21 07:59:34 -0500

    [fixed] error: 'children' is missing in props validationeslintreact/prop-types

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types";
+
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col items-center bg-black/90">{children}</div>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
